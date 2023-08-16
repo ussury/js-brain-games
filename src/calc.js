733f87712135661041ead84e12119e4938fa9a23
ussury <meshalkins@ya.ru>
2023-08-16 11:25:39 +0300

    fix Eslint

--- src/calc.js
+++ src/calc.js
@@ -1,4 +1,4 @@
-import gameLogic from "./gameLogic.js";
+import gameLogic from './gameLogic.js';
 
 export default (name) => {
   console.log('What is the result of the expression?');
@@ -6,17 +6,19 @@ export default (name) => {
     const firstNum = Math.floor(Math.random() * 49);
     const secondNum = Math.floor(Math.random() * 10);
     const symbols = ['*', '-', '+'];
-    const symbol = symbols[Math.floor(Math.random()*symbols.length)];
-    const getExpression = (symbol) => {
-      if (symbol === '*') {
-        return firstNum * secondNum;
+    const symbol = symbols[Math.floor(Math.random() * symbols.length)];
+    const getExpression = (arg) => {
+      const result = [];
+      if (arg === '*') {
+        result.push(firstNum * secondNum);
       }
-      if (symbol === '+') {
-        return firstNum + secondNum;
+      if (arg === '+') {
+        result.push(firstNum + secondNum);
       }
-      if (symbol === '-') {
-        return firstNum - secondNum;
+      if (arg === '-') {
+        result.push(firstNum - secondNum);
       }
+      return result[0];
     };
     const resultExpression = String(getExpression(symbol));
     const strExpression = `${String(firstNum)} ${symbol} ${String(secondNum)}`;
--- src/isEven.js
+++ src/isEven.js
@@ -1,14 +1,13 @@
-import gameLogic from "./gameLogic.js";
+import gameLogic from './gameLogic.js';
 
 export default (name) => {
   console.log('Answer "yes" if the number is even, otherwise answer "no"');
   const getRandomExpression = () => {
     const result = Math.floor(Math.random() * 99);
     const num = result === 0 ? 1 : result;
-    const isEven = (num) => num % 2 === 0;
+    const isEven = (arg) => arg % 2 === 0;
     const answer = isEven(num) ? 'yes' : 'no';
     return [answer, num];
   };
   return gameLogic(name, getRandomExpression);
 };
-
